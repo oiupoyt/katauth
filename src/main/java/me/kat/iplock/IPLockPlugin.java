686e40d0928c0d8d179ff4aa42f82108e0b0f96b
lighscent <augustin.pro.prs@gmail.com>
2026-02-12 17:15:49 +0100

    Implement LogManager for enhanced logging and update PreLoginListener to utilize it

--- src/main/java/me/kat/iplock/IPLockPlugin.java
+++ src/main/java/me/kat/iplock/IPLockPlugin.java
@@ -2,12 +2,14 @@ package me.kat.iplock;
 
 import me.kat.iplock.listener.PreLoginListener;
 import me.kat.iplock.storage.IPStorage;
+import me.kat.iplock.util.LogManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IPLockPlugin extends JavaPlugin {
 
     private static IPLockPlugin instance;
     private IPStorage storage;
+    private LogManager logManager;
 
     @Override
     public void onEnable() {
@@ -17,8 +19,10 @@ public class IPLockPlugin extends JavaPlugin {
         storage = new IPStorage(this);
         storage.load();
 
+        logManager = new LogManager(this);
+
         getServer().getPluginManager().registerEvents(
-                new PreLoginListener(storage), this);
+                new PreLoginListener(storage, logManager), this);
 
         getCommand("ipreset").setExecutor(new me.kat.iplock.command.IPResetCommand(storage));
         getCommand("ipstatus").setExecutor(new me.kat.iplock.command.IPStatusCommand(storage));
